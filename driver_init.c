df2c7483774e8030dfcf2176d2a9c6b35a17b605
Joshua Lynch <josh@codeposse.com>
2020-01-04 15:04:20 -0600

    initial import

---
+++ driver_init.c
@@ -0,0 +1,109 @@
+/*
+ * Code generated from Atmel Start.
+ *
+ * This file will be overwritten when reconfiguring your Atmel Start project.
+ * Please copy examples or other code you want to keep to a separate file
+ * to avoid losing it when reconfiguring.
+ */
+
+#include "driver_init.h"
+#include <peripheral_clk_config.h>
+#include <utils.h>
+#include <hal_init.h>
+
+#include <hpl_adc_base.h>
+
+/* The channel amount for ADC */
+#define ADC_0_CH_AMOUNT 1
+
+/* The buffer size for ADC */
+#define ADC_0_BUFFER_SIZE 16
+
+/* The maximal channel number of enabled channels */
+#define ADC_0_CH_MAX 0
+
+struct adc_async_descriptor         ADC_0;
+struct adc_async_channel_descriptor ADC_0_ch[ADC_0_CH_AMOUNT];
+
+static uint8_t ADC_0_buffer[ADC_0_BUFFER_SIZE];
+static uint8_t ADC_0_map[ADC_0_CH_MAX + 1];
+
+struct spi_m_async_descriptor SPI_0;
+
+/**
+ * \brief ADC initialization function
+ *
+ * Enables ADC peripheral, clocks and initializes ADC driver
+ */
+void ADC_0_init(void)
+{
+	hri_mclk_set_APBDMASK_ADC0_bit(MCLK);
+	hri_gclk_write_PCHCTRL_reg(GCLK, ADC0_GCLK_ID, CONF_GCLK_ADC0_SRC | (1 << GCLK_PCHCTRL_CHEN_Pos));
+	adc_async_init(&ADC_0, ADC0, ADC_0_map, ADC_0_CH_MAX, ADC_0_CH_AMOUNT, &ADC_0_ch[0], (void *)NULL);
+	adc_async_register_channel_buffer(&ADC_0, 0, ADC_0_buffer, ADC_0_BUFFER_SIZE);
+}
+
+void SPI_0_PORT_init(void)
+{
+
+	gpio_set_pin_level(PC04,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   false);
+
+	// Set pin direction to output
+	gpio_set_pin_direction(PC04, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_function(PC04, PINMUX_PC04C_SERCOM6_PAD0);
+
+	gpio_set_pin_level(PC05,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   false);
+
+	// Set pin direction to output
+	gpio_set_pin_direction(PC05, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_function(PC05, PINMUX_PC05C_SERCOM6_PAD1);
+
+	// Set pin direction to input
+	gpio_set_pin_direction(PC07, GPIO_DIRECTION_IN);
+
+	gpio_set_pin_pull_mode(PC07,
+	                       // <y> Pull configuration
+	                       // <id> pad_pull_config
+	                       // <GPIO_PULL_OFF"> Off
+	                       // <GPIO_PULL_UP"> Pull-up
+	                       // <GPIO_PULL_DOWN"> Pull-down
+	                       GPIO_PULL_OFF);
+
+	gpio_set_pin_function(PC07, PINMUX_PC07C_SERCOM6_PAD3);
+}
+
+void SPI_0_CLOCK_init(void)
+{
+	hri_gclk_write_PCHCTRL_reg(GCLK, SERCOM6_GCLK_ID_CORE, CONF_GCLK_SERCOM6_CORE_SRC | (1 << GCLK_PCHCTRL_CHEN_Pos));
+	hri_gclk_write_PCHCTRL_reg(GCLK, SERCOM6_GCLK_ID_SLOW, CONF_GCLK_SERCOM6_SLOW_SRC | (1 << GCLK_PCHCTRL_CHEN_Pos));
+
+	hri_mclk_set_APBDMASK_SERCOM6_bit(MCLK);
+}
+
+void SPI_0_init(void)
+{
+	SPI_0_CLOCK_init();
+	spi_m_async_init(&SPI_0, SERCOM6);
+	SPI_0_PORT_init();
+}
+
+void system_init(void)
+{
+	init_mcu();
+
+	ADC_0_init();
+
+	SPI_0_init();
+}
