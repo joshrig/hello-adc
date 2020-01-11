79222dcd11cf13dbfb1fb2846004ec53c04f3198
Joshua Lynch <josh@codeposse.com>
2020-01-11 17:26:12 -0600

    make the uart work. forcefully.

--- src/uart.c
+++ src/uart.c
@@ -10,10 +10,6 @@
 typedef struct
 {
     uint8_t *buf;
-    uint32_t wc;
-    uint32_t rc;
-
-    bool inprogress;
 
     struct usart_async_descriptor *uart;
     struct io_descriptor          *io;
@@ -29,15 +25,11 @@ static uint8_t        buffer[kMaxBufferSize];
 
 static void tx_cb_USART_0(const struct usart_async_descriptor *const io_descr)
 {
+}
+
+static void rx_cb_USART_0(const struct usart_async_descriptor *const io_descr)
+{
     uart_state_t *me = &uart_state;
-
-    me->inprogress = false;
-
-    if (me->wc > me->rc)
-    {
-        io_write(me->io, &me->buf[me->rc], me->wc - me->rc);
-        me->rc = me->wc;
-    }
 }
 
 
@@ -51,48 +43,30 @@ void uart_init(void)
 
     
     usart_async_register_callback(&USART_0, USART_ASYNC_TXC_CB, tx_cb_USART_0);
+    usart_async_register_callback(&USART_0, USART_ASYNC_RXC_CB, rx_cb_USART_0);
     usart_async_get_io_descriptor(&USART_0, &io);
     usart_async_enable(&USART_0);
 
     me->buf  = &buffer[0];
-    me->wc   = 0;
-    me->rc   = 0;
     me->uart = &USART_0;
     me->io   = io;
-    me->inprogress = false;
 }
+
 
 void uart_write(const char *str)
 {
-    int len;
+    int len = strlen(str);
     uart_state_t *me = &uart_state;
 
 
-    len = strlen(str);
-    if (me->wc + len > kMaxBufferSize)
-    {
-        int ncpy = kMaxBufferSize - (me->wc + len);
-        memcpy(&me->buf[me->wc], str, ncpy);
+    // wait for the current write to complete, if any
+    while (!usart_async_is_tx_empty(me->uart));
+    while (!usart_async_is_tx_empty(me->uart));
+    while (!usart_async_is_tx_empty(me->uart));
+    while (!usart_async_is_tx_empty(me->uart));
+    while (!usart_async_is_tx_empty(me->uart));
+    while (!usart_async_is_tx_empty(me->uart));
 
-        if (!me->inprogress)
-        {
-            me->inprogress = true;
-            io_write(me->io, &me->buf[me->rc], ncpy);
-            me->rc += ncpy;
-        }
-
-        memcpy(me->buf, str + ncpy, strlen(str) - ncpy);
-        me->wc = strlen(str) - ncpy;
-
-        return;
-    }
-
-    memcpy(&me->buf[me->wc], str, strlen(str));
-    me->wc += len;
-    if (!me->inprogress)
-    {
-        me->inprogress = true;
-        io_write(me->io, &me->buf[me->rc], len);
-        me->rc += len;
-    }
+    memcpy(me->buf, str, len);
+    io_write(me->io, me->buf, len);
 }
